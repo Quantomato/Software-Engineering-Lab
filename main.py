71f87d77da369cea6935c88d160e90e18c6315c8
mikey241124 <48805730+mikey241124@users.noreply.github.com>
2024-04-11 11:14:32 -0400

    L

--- main.py
+++ main.py
@@ -6,9 +6,26 @@ def encode(password):
     print(new_password)
     return int(new_password)
 
+
+def decode(password):
+    decoded = ''
+    decoding = []
+    for i in range(0, len(password)):
+        decoding.append(decoded + str(int(password[i:i + 1]) - 3))
+    for i in range(0,len(password)):
+        if decoding[i] == 0:
+            decoded = decoded + str(7)
+        elif decoding[i] == 1:
+            decoded = decoded + str(8)
+        elif decoding[i] == 2:
+            decoded = decoded + str(9)
+        else:
+            decoded = decoded + str(decoded[i])
+        return decoded
+
 def main():
     password = 0
-    #Print Menu
+    # Print Menu
     print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
     try:
         choice = int(input("Please enter an option: "))
@@ -16,7 +33,7 @@ def main():
         print("Invalid Input! Please try again.")
     else:
         if choice == 1:
-            #Ask user for proper input
+            # Ask user for proper input
             while True:
                 try:
                     password = int(input("Please enter your password to encode: "))
@@ -27,13 +44,14 @@ def main():
                 else:
                     break
         elif choice == 2:
-            #Decode
+            # Decode
             print(f"The encoded password is {password}, and the original password is {decode(password)}.\n")
             pass
         elif choice == 3:
             exit()
         else:
             print("Invalid Input!")
+
 
 if __name__ == "__main__":
     while True:
